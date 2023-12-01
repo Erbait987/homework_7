68b219af1a90b8bc67e5e7f9ae31e0d2fe8eee61
Erbait987 <erbikaiev@gmail.com>
2023-12-01 22:37:16 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useCallback, useState, useEffect } from "react";
+import React from "react";
+import Count from "./components/Count";
+import IsFive from "./components/IsFive";
+import "./App.css";
 
 function App() {
+  const [firstCount, setFirstCount] = useState(0);
+  const [secondCount, setSecondCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2>Пример с использованием хука UseMemo</h2>
+      <h3>Первый счетчик: </h3>
+      <div>
+        <button
+          onClick={() => {
+            setFirstCount(firstCount + 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </button>
+        <Count value={firstCount} id={1} />
+      </div>
+      <h3>Второй счетчик: </h3>
+      <div>
+        <button
+          onClick={() => {
+            setSecondCount(secondCount + 1);
+          }}
+        >
+          -
+        </button>
+        <Count value={secondCount} id={2} />
+        <IsFive value={secondCount} />
+      </div>
     </div>
   );
 }
